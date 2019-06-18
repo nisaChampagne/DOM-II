d5fcafa9bfc3b3475af3af5409bf59a1410d2ab8
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-06-18 14:57:52 -0400

    readme and laid out events going to use

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+//mouseover-- 1
+
+////mouseout---2
+
+///click---3
+
+///resize---4
+
+///copy---5
+
+///dragstart---6
+
+//dragend---7
+
+//dblclick---8
+
+///mouse enter --9
+
+//mouse leave--10
