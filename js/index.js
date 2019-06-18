c4836597b62251e4a991e4fd831c6ed324a9a5d2
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-06-18 15:25:54 -0400

    2 events done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
 // Your code goes here
 
-//mouseover-- 1
+//mouseover-- 1---nav a
+
+const navLinks = document.querySelector('a.nav-link');
+console.log('navLinks', navLinks);
+
+
+navLinks.addEventListener('mouseover', event =>{
+    event.target.style.color = 'green';
+    event.target.style.backgroundColor = 'yellow';
+
+    setTimeout(function(){
+        event.target.style.color = '';
+    }, 500);
+}, false);
+
 
 ////mouseout---2
 
+const secondaryHeader = document.querySelector('h2');
+console.log('secondaryHeader', secondaryHeader);
+
+secondaryHeader.addEventListener('mouseover', event =>{
+    event.target.style.color = 'purple';
+    
+    setTimeout(function(){
+        event.target.style.color = '';
+    }, 500);
+}, false);
+
 ///click---3
+
+
 
 ///resize---4
 
