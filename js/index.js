93aa961cc62e9a30838964488346ae525867a65c
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-07-09 11:44:03 -0400

    2 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+// []`mouseover` *
+
+const navs =  document.querySelectorAll('a.nav-link');
+console.log('navs', navs);
+
+navs.forEach((element) => 
+element.addEventListener('mouseover', event =>{
+    event.target.style.color = 'purple';
+
+    setTimeout(function(){
+        event.target.style.color = 'blue';
+    }, 1000);
+}))
+
+
+
+// []`click` *
+
+const funbus = document.querySelector('.logo-heading');
+console.log('funbus', funbus);
+
+funbus.addEventListener('click', event => {
+    event.target.style.color = 'aqua';
+
+    setTimeout(function(){
+        event.target.style.color = '';
+    }, 800)
+})
+
+
+
+
+
+// []`wheel` *
+// []`drag / drop` *
+// []`load` *
+// []`focus` *
+// []`resize` *
+// []`select` *
+// []`dblclick`;
